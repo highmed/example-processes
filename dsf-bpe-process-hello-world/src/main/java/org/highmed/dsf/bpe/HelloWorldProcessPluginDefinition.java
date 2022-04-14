11c88bdb3c4338c9a034fa7f220b152d0c817414
Hauke Hund <hauke.hund@hs-heilbronn.de>
2022-04-14 15:50:57 +0200

    changed to new #{version} placeholder integrated #{date} placeholder
    fixed value of ActivityDefinition.name to match recommended pattern.
    
    #closes #20

--- dsf-bpe-process-hello-world/src/main/java/org/highmed/dsf/bpe/HelloWorldProcessPluginDefinition.java
+++ dsf-bpe-process-hello-world/src/main/java/org/highmed/dsf/bpe/HelloWorldProcessPluginDefinition.java
@@ -1,5 +1,6 @@
 package org.highmed.dsf.bpe;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,7 @@ import ca.uhn.fhir.context.FhirContext;
 public class HelloWorldProcessPluginDefinition implements ProcessPluginDefinition
 {
 	public static final String VERSION = "0.6.0";
+	public static final LocalDate RELEASE_DATE = LocalDate.of(2022, 4, 14);
 
 	@Override
 	public String getName()
@@ -28,6 +30,12 @@ public class HelloWorldProcessPluginDefinition implements ProcessPluginDefinitio
 	public String getVersion()
 	{
 		return VERSION;
+	}
+
+	@Override
+	public LocalDate getReleaseDate()
+	{
+		return RELEASE_DATE;
 	}
 
 	@Override
@@ -52,7 +60,8 @@ public class HelloWorldProcessPluginDefinition implements ProcessPluginDefinitio
 		Map<String, List<AbstractResource>> resourcesByProcessKeyAndVersion = Map.of("highmedorg_helloWorld/" + VERSION,
 				Arrays.asList(aHelloWorld, tHelloWorld));
 
-		return ResourceProvider.read(VERSION, () -> fhirContext.newXmlParser().setStripVersionsFromReferences(false),
-				classLoader, resolver, resourcesByProcessKeyAndVersion);
+		return ResourceProvider.read(VERSION, RELEASE_DATE,
+				() -> fhirContext.newXmlParser().setStripVersionsFromReferences(false), classLoader, resolver,
+				resourcesByProcessKeyAndVersion);
 	}
 }
